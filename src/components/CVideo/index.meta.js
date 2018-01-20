3353d73aa192951ece68204ecb452c7ca52724ab
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-01-20 12:40:12 +0100

    Add video options
    Ref T42559

--- src/components/CVideo/index.meta.js
+++ src/components/CVideo/index.meta.js
@@ -3,4 +3,36 @@ export default {
   type: 'video',
   name: 'Video',
   icon: 'theaters',
+  options: {
+    autoplay: {
+      type: 'check',
+      name: 'Autoplay',
+      value: false,
+    },
+    controls: {
+      type: 'check',
+      name: 'Controls',
+      value: false,
+    },
+    repeat: {
+      type: 'check',
+      name: 'Repeat',
+      value: false,
+    },
+    muted: {
+      type: 'check',
+      name: 'Muted',
+      value: false,
+    },
+    width: {
+      type: 'input',
+      name: 'Width',
+      value: null,
+    },
+    value: {
+      type: 'inputList',
+      name: 'Sources',
+      value: [],
+    },
+  },
 };
