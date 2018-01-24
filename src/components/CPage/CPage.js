677d754df47983632b1f93b324e18f1f81c35817
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-01-24 12:34:16 +0100

    Modify page root element

--- src/components/CPage/CPage.js
+++ src/components/CPage/CPage.js
@@ -31,7 +31,7 @@ export default {
     }
 
     return createElement(
-      'v-container',
+      'div',
       {
         staticClass: `c-page c-page-${_.kebabCase(context.definition.name)}`,
       },
