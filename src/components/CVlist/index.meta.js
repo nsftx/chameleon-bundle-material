ced9b66c6291fc85d193486a4793d0ef9bcc59ab
Lana <dzidic.lana@nsoft.com>
2018-01-25 17:23:11 +0100

    Update docs
    Ref T42465

--- src/components/CVlist/index.meta.js
+++ src/components/CVlist/index.meta.js
@@ -1,10 +1,54 @@
 /**
- * @name VerticalList
+ * @module Vertical List
+ * @typicalname CVlist
  *
- * Container that displays its children as a vertical list
- * Each child has set width to 100% of the Vertical list
- * @property {string} group Group of the container
- * @property {string} type Type of the container
+ *
+ * @desc Container that displays its children as a vertical list
+ * Each child has its width set to 100% of Vertical list
+ * @group containers
+ *
+ * @property {String} group Component group
+ * @property {String} type Component type used as a component tag
+ * @property {String} name Component name
+ * @property {String} icon Material icon name
+ * @property {Array} children List of possible children groups
+ * @property {Object} options
+ *
+ * @example {
+ *  group: 'containers',
+ *  type: 'vlist',
+ *  name: 'Vertical List',
+ *  icon: 'view_week',
+ *  children: [
+ *    'containers',
+ *    'widgets',
+ *  ],
+ *  options: {
+ *    color: {
+ *      type: 'input',
+ *      name: 'Color',
+ *      value: 'transparent',
+ *      validation: {
+ *        required: true,
+ *        minLength: 3,
+ *        maxLength: 25,
+ *      },
+ *      priority: 1,
+ *    },
+ *    flat: {
+ *      type: 'check',
+ *      name: 'No shadow',
+ *      value: false,
+ *      priority: 2,
+ *    },
+ *    gutter: {
+ *      type: 'check',
+ *      name: 'Spacing',
+ *      value: false,
+ *      priority: 3,
+ *    },
+ *  },
+ * }
  */
 
 export default {
@@ -16,7 +60,20 @@ export default {
     'containers',
     'widgets',
   ],
+
+  /**
+   * @type {Object}
+   * @desc Object containing options for component customization
+   */
   options: {
+    /**
+     * @type {Object}
+     * @property {String} type Color input field type
+     * @property {String} name Color input field name/placeholder
+     * @property {String} value Color input field default value
+     * @property {Object} validation Validation rules for color input field
+     * @property {Number} priority Color input field priority
+     */
     color: {
       type: 'input',
       name: 'Color',
@@ -28,12 +85,30 @@ export default {
       },
       priority: 1,
     },
+
+    /**
+     * @type {Object}
+     * @property {String} type Flat input field type
+     * @property {String} name Flat input field name/placeholder
+     * @property {String} value Flat input field default value
+     * @property {Object} validation Validation rules for flat input field
+     * @property {Number} priority Flat input field priority
+     */
     flat: {
       type: 'check',
       name: 'No shadow',
       value: false,
       priority: 2,
     },
+
+    /**
+     * @type {Object}
+     * @property {String} type Gutter input field type
+     * @property {String} name Gutter input field name/placeholder
+     * @property {String} value Gutter input field default value
+     * @property {Object} validation Validation rules for gutter input field
+     * @property {Number} priority Gutter input field priority
+     */
     gutter: {
       type: 'check',
       name: 'Spacing',
