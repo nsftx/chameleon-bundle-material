b32d128ad188ac683a4095a1b7e2ca7182166421
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-01-31 14:46:01 +0100

    Add bundle
    Ref T43340

--- src/index.meta.js
+++ src/index.meta.js
@@ -1,1 +1,35 @@
-export * from './components/index.meta.js';
+import * as elements from './components/index.meta.js';
+
+const elementGroups = {
+  containers: {
+    key: 'containers',
+    name: 'Containers',
+  },
+  widgets: {
+    key: 'widgets',
+    name: 'Widgets',
+  },
+  inputs: {
+    key: 'inputs',
+    name: 'Inputs',
+  },
+  other: {
+    key: 'other',
+    name: 'Other',
+  },
+};
+
+const optionGroups = {
+  general: {
+    key: 'general',
+    name: 'General',
+  },
+};
+
+export default {
+  name: 'material',
+  title: 'Material Bundle',
+  elementGroups,
+  elements,
+  optionGroups,
+}
