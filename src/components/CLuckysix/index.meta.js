eb672e89e2a4253f340e83fae800f9328f675ab6
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-02-01 09:41:16 +0100

    Add additional L6 options
    Ref T43348

--- src/components/CLuckysix/index.meta.js
+++ src/components/CLuckysix/index.meta.js
@@ -4,31 +4,93 @@ export default {
   name: 'LuckySix',
   icon: 'group_work',
   options: {
+    color: {
+      type: 'select',
+      name: 'Color',
+      items: [
+        {
+          name: 'Indigo Blue',
+          value: 230,
+        },
+        {
+          name: 'Marine Blue',
+          value: 215,
+        },
+        {
+          name: 'Moulin Rouge',
+          value: 340,
+        },
+        {
+          name: 'Pepper Red',
+          value: 0,
+        },
+        {
+          name: 'Submarine Yellow',
+          value: 40,
+        },
+        {
+          name: 'Spring Green',
+          value: 120,
+        },
+        {
+          name: 'Polar Green',
+          value: 150,
+        },
+      ],
+      value: 230,
+      priority: 1,
+    },
+    drum: {
+      type: 'select',
+      name: 'Drum Set',
+      items: [
+        {
+          name: 'Solids',
+          value: 'solids',
+        },
+        {
+          name: 'Stripes',
+          value: 'stripes',
+        },
+      ],
+      value: 'solids',
+      priority: 2,
+    },
     quality: {
       type: 'select',
       name: 'Quality',
       items: [
         {
-          id: 1,
           name: 'Low',
           value: 'low',
         },
         {
-          id: 2,
           name: 'Medium',
           value: 'medium',
         },
         {
-          id: 3,
           name: 'High',
           value: 'high',
         },
       ],
-      returnObject: false,
-      displayProp: 'name',
-      valueProp: 'value',
       value: 'low',
-      priority: 1,
+      priority: 3,
+    },
+    language: {
+      type: 'select',
+      name: 'Language',
+      items: [
+        {
+          name: 'English',
+          value: 'en',
+        },
+        {
+          name: 'Serbian Latin',
+          value: 'sr-Latn',
+        },
+      ],
+      value: 'en',
+      priority: 3,
     },
   },
 };
