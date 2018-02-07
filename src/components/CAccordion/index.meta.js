e085bdc52e463a9a4e9f04b5267f61d6177b1328
Lana <dzidic.lana@nsoft.com>
2018-02-07 12:43:04 +0100

    Add accordion meta options
    Ref T43333

--- src/components/CAccordion/index.meta.js
+++ src/components/CAccordion/index.meta.js
@@ -6,5 +6,82 @@ export default {
   children: [
     'accordionItem',
   ],
-  options: {},
+  options: {
+    alternativeDesign: {
+      type: 'select',
+      name: 'Alternative design',
+      items: [
+        {
+          id: 1,
+          name: '',
+          value: '',
+        },
+        {
+          id: 2,
+          name: 'Inset',
+          value: 'inset',
+        },
+        {
+          id: 3,
+          name: 'Popout',
+          value: 'popout',
+        },
+      ],
+      returnObject: false,
+      displayProp: 'name',
+      valueProp: 'value',
+      value: [],
+      priority: 4,
+    },
+    itemsCount: {
+      type: 'input',
+      name: 'Item count',
+      value: 1,
+      validation: {
+        required: true,
+        min: 1,
+      },
+      priority: 1,
+    },
+    color: {
+      type: 'input',
+      name: 'Item header color',
+      value: 'transparent',
+      validation: {
+        required: true,
+        minLength: 3,
+        maxLength: 25,
+      },
+      priority: 2,
+    },
+    contentColor: {
+      type: 'input',
+      name: 'Item content color',
+      value: 'transparent',
+      validation: {
+        required: true,
+        minLength: 3,
+        maxLength: 25,
+      },
+      priority: 3,
+    },
+    multipleExpand: {
+      type: 'check',
+      name: 'Multiple expanded items',
+      value: false,
+      priority: 5,
+    },
+    contentRipple: {
+      type: 'check',
+      name: 'Ripple on item header',
+      value: false,
+      priority: 6,
+    },
+    hideItemActions: {
+      type: 'check',
+      name: 'Hide item actions',
+      value: false,
+      priority: 7,
+    },
+  },
 };
