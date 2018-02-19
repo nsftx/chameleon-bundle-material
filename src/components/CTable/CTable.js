8339b643c3b2698e9139b86bde851b21200f6417
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-02-19 18:00:48 +0100

    Add inferred props
    Ref T44122

--- src/components/CTable/CTable.js
+++ src/components/CTable/CTable.js
@@ -1,5 +1,5 @@
 import namespace from '@namespace';
-import { each, isNil, keys, map } from 'lodash';
+import { each, isNil, keys, map, merge } from 'lodash';
 import { localizable } from '@mixins';
 
 const getAttrs = (context) => {
@@ -10,13 +10,44 @@ const getAttrs = (context) => {
   return attrs;
 };
 
-const getScopedSlots = (createElement) => {
+const getScopedSlots = (createElement, dataSource) => {
   const getColumns = (props) => {
     const item = props.item;
     const columns = [];
 
     each(keys(item), (key) => {
-      columns.push(createElement('td', {}, item[key]));
+      let content = item[key];
+      if (dataSource && dataSource.columns) {
+        const column = dataSource.columns[key];
+        if (column) {
+          switch (column.type) {
+            case 'icon':
+              content = [createElement('v-icon', content)];
+              break;
+            case 'image':
+              content = [
+                createElement('v-avatar', {
+                  attrs: {
+                    // NOTE: Expose in options?
+                    size: '32px',
+                  },
+                },
+                  [
+                    createElement('img', {
+                      attrs: {
+                        src: content,
+                      },
+                    }),
+                  ]),
+              ];
+              break;
+            default:
+              content = item[key];
+          }
+        }
+      }
+
+      columns.push(createElement('td', {}, content));
     });
 
     return columns;
@@ -29,12 +60,35 @@ const getScopedSlots = (createElement) => {
   return slot;
 };
 
+const getCellInferredProps = (column) => {
+  let align = column.align;
+
+  if (!align) {
+    switch (column.type) {
+      case 'date':
+      case 'icon':
+      case 'image':
+        align = 'center';
+        break;
+      case 'number':
+        align = 'right';
+        break;
+      default:
+        align = 'left';
+    }
+  }
+
+  return {
+    align,
+  };
+};
+
 const getHeadersProp = (dataSource) => {
   const columns = dataSource.columns;
-  return map(columns, column => ({
+  return map(columns, column => (merge({
     value: column.name,
     text: column.title,
-  }));
+  }, getCellInferredProps(column))));
 };
 
 const getProps = (context) => {
@@ -70,12 +124,14 @@ export default {
     },
   },
   render(createElement) {
+    const dataSource = this.definition.dataSource;
+
     return createElement(
       'v-data-table',
       {
         attrs: getAttrs(this),
         props: getProps(this),
-        scopedSlots: getScopedSlots(createElement),
+        scopedSlots: getScopedSlots(createElement, dataSource),
         staticClass: this.$options.name,
         class: [
           this.definition.color || 'white',
