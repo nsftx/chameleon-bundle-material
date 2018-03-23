0e0b067c8bd9196799e77582c8f10ff8ad22ea8b
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-03-23 11:37:04 +0100

    Update SDK

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,6 +1,4 @@
 export { default as fieldable } from './fieldable';
 export { default as localizable } from './localizable';
-export { default as sourceable } from './sourceable';
 export { default as validatable } from './validatable';
-export { dependable } from 'chameleon-sdk/src/mixins';
-export { elementable } from 'chameleon-sdk/src/mixins';
+export { dependable, elementable, sourceable } from 'chameleon-sdk/src/mixins';
