2edd4d71eac5798e1dfab720037514e179a71b12
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-04-24 09:41:30 +0200

    Fix dependable

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,4 +1,15 @@
 export { default as fieldable } from './fieldable';
 export { default as localizable } from './localizable';
 export { default as validatable } from './validatable';
-export { bindable, dependable, elementable, reactionable, sourceable } from '@nsoft/chameleon-sdk/src/mixins';
+
+/*
+To test it by directly changing SDK,
+change path to ../../../chameleon-sdk/src/mixins
+*/
+export {
+  bindable,
+  dependable,
+  elementable,
+  reactionable,
+  sourceable,
+} from '@nsoft/chameleon-sdk/src/mixins';
