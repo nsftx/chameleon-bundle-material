450f490139341bd65276ebe0d31b63af9c3592b6
Snjezana Dreznjak <dreznjak.snjezana@nsoft.com>
2018-05-03 12:18:10 +0200

    Add meta options
    resolves  #5

--- src/components/CPage/index.meta.js
+++ src/components/CPage/index.meta.js
@@ -1,5 +1,55 @@
 export default {
   type: 'page',
+  name: 'Page',
+  icon: 'browser',
+  optionGroups: {
+    meta: {
+      key: 'meta',
+      name: 'Meta',
+    },
+  },
+  options: {
+    name: {
+      type: 'input',
+      name: 'Name',
+      value: null,
+      priority: 1,
+    },
+    path: {
+      type: 'input',
+      name: 'Path',
+      value: '/',
+      priority: 2,
+    },
+    parent: {
+      type: 'select',
+      name: 'Parent',
+      value: null,
+      displayProp: 'name',
+      valueProp: 'name',
+      items: [],
+      priority: 3,
+    },
+    meta: {
+      type: 'group',
+      group: 'meta',
+      title: {
+        type: 'input',
+        name: 'Title',
+        value: null,
+      },
+      description: {
+        type: 'input',
+        name: 'Description',
+        value: null,
+      },
+      keywords: {
+        type: 'input',
+        name: 'Keywords',
+        value: null,
+      },
+    },
+  },
   actions: [
     {
       name: 'navigateToPage',
@@ -18,6 +68,4 @@ export default {
   events: [
     { name: 'Loading' },
   ],
-  options: null,
-  hidden: true,
 };
