695c611f30b25c889cb970467a2f5a5fe90b48c8
Lana <dzidic.lana@nsoft.com>
2018-09-12 17:08:01 +0200

    Don't load items on autoGenerate changes #87

--- src/components/CMenu/CMenu.js
+++ src/components/CMenu/CMenu.js
@@ -46,17 +46,13 @@ export default {
     selectItem(item) {
       this.sendToEventBus('SelectedItemChanged', item);
     },
-    setItemsOrLoad() {
-      if (this.config.autoGenerate) {
-        const pages = this.getBindingValue('=$app.pages');
-        this.items = map(pages, page => ({
-          icon: page.icon,
-          label: page.meta.title,
-          path: page.path,
-        }));
-      } else {
-        this.loadData();
-      }
+    setItems() {
+      const pages = this.getBindingValue('=$app.pages');
+      this.items = map(pages, page => ({
+        icon: page.icon,
+        label: page.meta.title,
+        path: page.path,
+      }));
     },
     renderDivider() {
       return this.$createElement(
@@ -182,8 +178,8 @@ export default {
         this.loadData();
       }
     },
-    autoGenerate() {
-      this.setItemsOrLoad();
+    autoGenerate(val) {
+      if (val) this.setItems();
     },
   },
   render(createElement) {
