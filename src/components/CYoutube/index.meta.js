effa8e1c3940d1a4d8fe9e68ee0719789395d717
Snjezana Dreznjak <dreznjak.snjezana@nsoft.com>
2019-01-18 12:38:35 +0100

    Refactor youtube and add dataSource
    ref #159

--- src/components/CYoutube/index.meta.js
+++ src/components/CYoutube/index.meta.js
@@ -1,9 +1,29 @@
+import { binding } from '@nsoft/chameleon-sdk/src/utility';
+
+const itemInterface = [
+  {
+    name: 'url',
+    type: 'String',
+    label: 'Url',
+  },
+];
+
 export default {
   group: 'widgets',
   type: 'youtube',
   name: 'YouTube',
   icon: 'ondemand_video',
+  optionGroups: {
+    data: {
+      key: 'data',
+      name: 'Data',
+    },
+  },
   actions: [
+    {
+      name: 'setDataSource',
+      help: 'Sets table data source from event data',
+    },
     {
       name: 'play',
       help: 'Start playing',
@@ -18,6 +38,10 @@ export default {
     },
   ],
   events: [
+    {
+      name: 'DataSourceChanged',
+      help: 'Fires when table data source is changed',
+    },
     {
       name: 'PlayerReadyChanged',
       help: 'Video ready state changed',
@@ -72,21 +96,11 @@ export default {
       items: [
         {
           id: 1,
-          name: 'Video',
-          value: '',
-        },
-        {
-          id: 2,
           name: 'Playlist',
           value: 'playlist',
         },
         {
           id: 3,
-          name: 'User Uploads',
-          value: 'user_uploads',
-        },
-        {
-          id: 4,
           name: 'Search Results',
           value: 'search',
         },
@@ -94,7 +108,7 @@ export default {
       returnObject: false,
       displayProp: 'name',
       valueProp: 'value',
-      value: '',
+      value: 'playlist',
       priority: 1,
     },
     repeat: {
@@ -106,8 +120,27 @@ export default {
     value: {
       type: 'inputList',
       name: 'Video/Playlist Source',
-      value: [],
+      group: 'data',
+      value: null,
+      disabled: {
+        current: false,
+        default: false,
+        expression: binding.setExpression('<%= element.dataSource != null %>'),
+      },
       priority: 2,
+    },
+    dataSource: {
+      type: 'dataSource',
+      group: 'data',
+      name: 'Data Source',
+      value: null,
+      schema: itemInterface,
+      disabled: {
+        current: false,
+        default: false,
+        expression: binding.setExpression('<%= element.value != null && element.value.length > 0 %>'),
+      },
+      priority: 1,
     },
   },
 };
