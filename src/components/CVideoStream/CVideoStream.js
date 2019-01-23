4c5e51d72aa729e19a08065cc858d2c458876a58
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2019-01-23 12:10:33 +0100

    Support MPEG-1 streams #163

--- src/components/CVideoStream/CVideoStream.js
+++ src/components/CVideoStream/CVideoStream.js
@@ -1,71 +1,133 @@
-import { isNil, isObject } from 'lodash';
+import { isNil, isObject, template, upperFirst } from 'lodash';
 import Element from '../Element';
-
-const getStreamType = (config) => {
-  const type = isNil(config.streamType) ? 'img' : config.streamType;
-  return type;
-};
-
-const getStaticStyle = (config) => {
-  let height = 'auto';
-  let paddingTop = 0;
-
-  if (config.aspectRatio !== 'auto' && !isNil(config.aspectRatio)) {
-    height = 0;
-    const ratioValue = config.aspectRatio.split(':');
-    paddingTop = `${(ratioValue[1] / ratioValue[0]) * 100}%`;
-  }
-
-  const style = {
-    position: 'relative',
-    overflow: 'hidden',
-    width: '100%',
-    height,
-    paddingTop,
-  };
-
-  return style;
-};
 
 export default {
   extends: Element,
+  data() {
+    return {
+      player: null,
+      playerHandler: null,
+    };
+  },
+  computed: {
+    dependencies() {
+      if (this.streamType === 'Jsmpeg') {
+        return {
+          files: ['https://jsmpeg.com/jsmpeg.min.js'],
+          global: 'JSMpeg',
+        };
+      }
+
+      return null;
+    },
+    staticStyle() {
+      let height = 'auto';
+      let paddingTop = 0;
+
+      if (this.config.aspectRatio !== 'auto' && !isNil(this.config.aspectRatio)) {
+        height = 0;
+        const ratioValue = this.config.aspectRatio.split(':');
+        paddingTop = `${(ratioValue[1] / ratioValue[0]) * 100}%`;
+      }
+
+      const style = {
+        position: 'relative',
+        overflow: 'hidden',
+        width: '100%',
+        height,
+        paddingTop,
+      };
+
+      return style;
+    },
+    streamValue() {
+      if (this.items && this.items.length) {
+        const isMasked = !isNil(this.config.valueMask);
+        const isValueObject = isObject(this.items[0]);
+        const value = isValueObject ? this.items[0].url : this.items[0];
+
+        return isMasked && isValueObject ? template(this.config.valueMask)(this.items[0]) : value;
+      }
+
+      return this.config.value;
+    },
+    streamType() {
+      let type = isNil(this.config.streamType) ? 'image' : this.config.streamType;
+
+      if (type === 'img') type = 'image';
+      if (type === 'mpeg') type = 'jsmpeg';
+
+      return upperFirst(type);
+    },
+  },
+  methods: {
+    createPlayer() {
+      if (this.streamValue) {
+        this.player = new window[`${this.playerHandler}`].Player(this.streamValue, {
+          canvas: this.$refs.canvas,
+        });
+
+        this.player.play();
+      }
+    },
+    renderTypeImage() {
+      return this.$createElement(
+        'img',
+        {
+          attrs: {
+            src: this.streamValue || null,
+            width: '100%',
+            height: '100%',
+          },
+          staticStyle: {
+            position: this.config.aspectRatio !== 'auto' ? 'absolute' : 'relative',
+            top: 0,
+            left: 0,
+          },
+        },
+      );
+    },
+    renderTypeJsmpeg() {
+      return this.$createElement(
+        'canvas',
+        {
+          ref: 'canvas',
+          staticStyle: {
+            position: this.config.aspectRatio !== 'auto' ? 'absolute' : 'relative',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          },
+        },
+      );
+    },
+  },
   watch: {
     dataSource: {
-      handler() {
-        this.loadData();
+      handler(current, previous) {
+        this.loadData().then(() => {
+          if (!isNil(previous) && !isNil(this.playerHandler)) {
+            this.createPlayer();
+          }
+        });
       },
       deep: true,
     },
   },
-  computed: {
-    streamValue() {
-      if (this.items && this.items.length) {
-        return isObject(this.items[0]) ? this.items[0].url : this.items[0];
-      }
-      return this.config.value;
-    },
+  mounted() {
+    if (this.dependencies) {
+      this.playerHandler = this.dependencies.global;
+      this.loadDependencies(this.dependencies.files, this.playerHandler).then(() => {
+        this.createPlayer();
+      });
+    }
   },
-  render(createElement) {
-    const data = {
-      staticStyle: getStaticStyle(this.config),
-    };
+  render() {
+    const children = this[`renderType${this.streamType}`]();
 
-    const children = createElement(
-      getStreamType(this.config),
-      {
-        attrs: {
-          src: this.streamValue || null,
-          width: '100%',
-          height: '100%',
-        },
-        staticStyle: {
-          position: this.config.aspectRatio !== 'auto' ? 'absolute' : 'relative',
-          top: 0,
-          left: 0,
-        },
-      },
-    );
-
-    return this.renderElement('div', data, children);
+    return this.renderElement('div', {
+      staticStyle: this.staticStyle,
+    }, children);
   },
 };
