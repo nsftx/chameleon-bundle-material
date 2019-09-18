e5a96aa920cc76b56d3969afbb13432e43096915
Lana <dzidic.lana@nsoft.com>
2019-09-18 14:35:55 +0200

    Use manifest components list in dev/playground mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,21 +1,51 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import { each } from 'lodash';
 import 'vuetify/dist/vuetify.min.css';
+import manifest from '../../build/manifest.json';
 
-Vue.use(Vuetify);
+const { components } = manifest.plugins.vuetify;
+components.push(
+  'VApp',
+  'VAppBar',
+  'VAppBarNavIcon',
+  'VToolbarTitle',
+  'VFooter',
+);
 
-export default new Vuetify({
-  icons: {
-    iconfont: 'md',
+const importComponents = () => {
+  const importPromise = import('vuetify/lib').then((vuetifyModule) => {
+    const vuetifyComponents = {};
+    each(components, (component) => {
+      vuetifyComponents[component] = vuetifyModule[component];
+    });
+
+    return vuetifyComponents;
+  });
+
+  return importPromise;
+};
+
+export default {
+  init() {
+    return importComponents().then((importedComponents) => {
+      Vue.use(Vuetify, { components: importedComponents });
+
+      return new Vuetify({
+        icons: {
+          iconfont: 'md',
+        },
+        theme: {
+          themes: {
+            light: {
+              primary: '#01a952',
+            },
+            dark: {
+              primary: '#01a952',
+            },
+          },
+        },
+      });
+    });
   },
-  theme: {
-    themes: {
-      light: {
-        primary: '#01a952',
-      },
-      dark: {
-        primary: '#01a952',
-      },
-    },
-  },
-});
+};
